a27c08cba0cb910e7a06e77b2f7d19823fd40ddb
Kailiangdong <kailiang.dong@tum.de>
2018-06-24 12:44:38 +0200

    commit saving process

--- frontend/src/services/AttractionService.js
+++ frontend/src/services/AttractionService.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import HttpService from './HttpService';
+import MovieService from "./MovieService";
 
 export default class AttractionService {
 
@@ -53,6 +54,29 @@ export default class AttractionService {
                 else {
                     reject('Error while retrieving attraction');
                 }
+            }, function(textStatus) {
+                reject(textStatus);
+            });
+        });
+    }
+    static getAttraction(id) {
+        return new Promise((resolve, reject) => {
+            HttpService.get(`${AttractionService.baseURL()}/pre/${id}`, function(data) {
+                if(data != undefined || Object.keys(data).length !== 0) {
+                    resolve(data);
+                }
+                else {
+                    reject('Error while retrieving movie');
+                }
+            }, function(textStatus) {
+                reject(textStatus);
+            });
+        });
+    }
+    static filterattraction(filterinfo) {
+        return new Promise((resolve, reject) => {
+            HttpService.post(`${AttractionService.baseURL()}/filter`, filterinfo, function(data) {
+                resolve(data);
             }, function(textStatus) {
                 reject(textStatus);
             });
