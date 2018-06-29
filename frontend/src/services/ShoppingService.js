cecc49bed57c5b891883d5ff6f32af132e12d14b
Jingzhou Wang <jingzhou.wang@tum.de>
2018-06-29 22:11:07 +0200

    shopping cart: work in progress

--- frontend/src/services/ShoppingService.js
+++ frontend/src/services/ShoppingService.js
@@ -9,7 +9,8 @@ export default class ShoppingService {
 
     static baseURL() {return "http://localhost:3000/cartOrder" }
 
-    static getTickets(){
+
+    static addTicketToCart(){
         return new Promise((resolve, reject) => {
             HttpService.get(this.baseURL(), function(data) {
                 resolve(data);
@@ -19,6 +20,7 @@ export default class ShoppingService {
         });
     }
 
+    //userID to load cart
     static getCart(id) {
         return new Promise((resolve, reject) => {
             HttpService.get(`${ShoppingService.baseURL()}/cart/${id}`, function(data) {
@@ -50,7 +52,7 @@ export default class ShoppingService {
     }
 
 
-    static deleteCartItem(id) {
+    static deleteCartTicket(id) {
         return new Promise((resolve, reject) => {
             HttpService.remove(`${TicketService.baseURL()}/${id}`, function(data) {
                 if(data.message != undefined) {
@@ -65,30 +67,4 @@ export default class ShoppingService {
         });
     }
 
-    static returnTicket(ticket) {
-        /*return new Promise((resolve, reject) => {
-            HttpService.put(`${this.baseURL()}/${movie._id}`, movie, function(data) {
-                resolve(data);
-            }, function(textStatus) {
-                reject(textStatus);
-            });
-        });*/
-    }
-
-    static createComments(ticket) {
-        ticket.id = Math.floor((Math.random() * 100000000) + 1).toString();
-        /*movie.posters = {
-            thumbnail: "http://resizing.flixster.com/AeDB8hgaGed_TMCcIF1P_gubGwA=/54x81/dkpu1ddg7pbsk.cloudfront.net/movie/11/27/63/11276344_ori.jpg",
-            profile: "http://resizing.flixster.com/AeDB8hgaGed_TMCcIF1P_gubGwA=/54x81/dkpu1ddg7pbsk.cloudfront.net/movie/11/27/63/11276344_ori.jpg",
-            detailed: "http://resizing.flixster.com/AeDB8hgaGed_TMCcIF1P_gubGwA=/54x81/dkpu1ddg7pbsk.cloudfront.net/movie/11/27/63/11276344_ori.jpg",
-            original: "http://resizing.flixster.com/AeDB8hgaGed_TMCcIF1P_gubGwA=/54x81/dkpu1ddg7pbsk.cloudfront.net/movie/11/27/63/11276344_ori.jpg"
-        };*/
-        return new Promise((resolve, reject) => {
-            HttpService.post(TicketService.baseURL(), movie, function(data) {
-                resolve(data);
-            }, function(textStatus) {
-                reject(textStatus);
-            });
-        });
-    }
 }
